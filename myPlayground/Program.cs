e9f062c6a9b60ba56f04f11081f2b97d0d929238
Tyler Morgan <clickjaw@outlook.com>
2022-11-02 17:35:02 -0500

    working

--- myPlayground/Program.cs
+++ myPlayground/Program.cs
@@ -6,10 +6,9 @@ namespace Playground
     {
         public int LiquidLevel;
 
-        public void Drink(int ml)
+        public void Drink(int milliliters)
         {
-
-            LiquidLevel -= ml;
+            LiquidLevel -= milliliters;
         }
 
         public void Refill()
@@ -39,26 +38,58 @@ namespace Playground
                 string command = Console.ReadLine();
 
                 string[] divide = command.Split(' ');
-               
+
                 switch (divide[0])
                 {
 
                     case "drink":
-                        
-                        string number = divide[1];
-                        int ml = Convert.ToInt32(number);
 
-                        glass.Drink(ml);
+                        string number = divide[1];
+                        int milliliters = Convert.ToInt32(number);
+
+                        glass.Drink(milliliters);
                         if (glass.LiquidLevel < 100)
                             glass.Refill();
                         break;
 
                     case "print":
 
-                        Console.WriteLine($"This glass contains {glass.LiquidLevel}ml of liquid.");
+                        if (glass.LiquidLevel >= 151 && glass.LiquidLevel < 200)
+                        {
+                            Console.WriteLine(@"
+|  |
+|##|
+|##|
+|##|
+
+
+                                ");
+                        }
+                        else if (glass.LiquidLevel <= 150 && glass.LiquidLevel >= 100)
+                        {
+                            Console.WriteLine(@"
+|  |
+|  |
+|##|
+|##|                   
+                                                    
+                                ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(@"
+|##|
+|##|
+|##|
+|##|
+
+                            ");
+                        }
+
+                        // Console.WriteLine($"This glass contains {glass.LiquidLevel} milliliters of liquid.");
                         break;
 
-                    case "exit":
+                    case "stop":
 
                         stopped = true;
                         break;
@@ -68,161 +99,3 @@ namespace Playground
         }//Main
     }//Program
 }
-
-
-
-
-
-
-
-
-
-
-// using System;
-// using System.Threading;
-// namespace Tic_Tac_Toe
-// {
-//     class Program
-//     {
-//         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-//         static int user = 1;
-//         static int userName;
-
-//         static int scoreCount = 0;
-//         static void Main(string[] args)
-//         {
-//             Console.Clear();
-//             Console.WriteLine("Enter name user1: ");
-//             string userNameSelection1 = Console.ReadLine();
-//             Console.WriteLine("Enter name user2: ");
-//             string userNameSelection2 = Console.ReadLine();
-//             Console.WriteLine($"{userNameSelection1}: X\n{userNameSelection2}: O");
-//             do
-//             {
-
-//                 Console.Clear(); // this calls a fresh window. love it.
-
-//                 Console.WriteLine("\n");
-//                 if (user % 2 == 0)// when user is called if modulus=0 then it's user1
-//                 {
-//                     Console.WriteLine($"{userNameSelection2}'s Chance");
-//                 }
-//                 else
-//                 {
-//                     Console.WriteLine($"{userNameSelection1}'s Chance");
-//                 }
-//                 Console.WriteLine("\n");
-
-//                 Board();
-
-//                 userName = int.Parse(Console.ReadLine());
-//                 //if arr[userName] == both X&O then it returns a "sorry"
-//                 if (arr[userName] != 'X' && arr[userName] != 'O')
-//                 {
-//                     if (user % 2 == 0)
-//                     {
-//                         arr[userName] = 'O';
-//                         user++;
-//                     }
-//                     else
-//                     {
-//                         arr[userName] = 'X';
-//                         user++;
-//                     }
-//                 }
-//                 else
-
-//                 {
-//                     Console.WriteLine("{0} is already taken by {1}.\nSelect again.", userName, arr[userName]);
-//                     Thread.Sleep(1500);
-//                 }
-
-//                 scoreCount = win();// calling win
-//             }
-//             while (scoreCount != 1 && scoreCount != -1);
-//             Console.Clear();// clearing the console
-//             Board();
-//             if (scoreCount == 1)
-//             {
-//                 if (userName == 1)
-//                 {
-//                     Console.Clear();
-//                     Console.WriteLine($"{userNameSelection1} wins!");
-//                 }
-//                 else
-//                 {
-//                     Console.Clear();
-//                     Console.WriteLine($"{userNameSelection1} wins!");
-//                 }
-//             }
-//             else
-//             {
-//                 Console.Clear();
-//                 Console.WriteLine("Draw");
-//             }
-//             Console.ReadLine();
-//         }
-//         // Board method which creats board
-//         private static void Board()
-//         {
-//             Console.WriteLine("     |     |      ");
-//             Console.WriteLine($"  {arr[1]}  |  {arr[2]}  |  {arr[3]}");
-//             Console.WriteLine("_____|_____|_____ ");
-//             Console.WriteLine("     |     |      ");
-//             Console.WriteLine($"  {arr[4]}  |  {arr[5]}  |  {arr[6]}");
-//             Console.WriteLine("_____|_____|_____ ");
-//             Console.WriteLine("     |     |      ");
-//             Console.WriteLine($"  {arr[7]}  |  {arr[8]}  |  {arr[9]}");
-//             Console.WriteLine($"     |     |      ");
-//         }
-
-
-//         private static int win()
-//         {
-//             // Horzontal Win
-//             if (arr[1] == arr[2] && arr[2] == arr[3])
-//             {
-//                 return 1;
-//             }
-//             else if (arr[4] == arr[5] && arr[5] == arr[6])
-//             {
-//                 return 1;
-//             }
-//             else if (arr[6] == arr[7] && arr[7] == arr[8])
-//             {
-//                 return 1;
-//             }
-//             // vertical Win
-//             else if (arr[1] == arr[4] && arr[4] == arr[7])
-//             {
-//                 return 1;
-//             }
-//             else if (arr[2] == arr[5] && arr[5] == arr[8])
-//             {
-//                 return 1;
-//             }
-//             else if (arr[3] == arr[6] && arr[6] == arr[9])
-//             {
-//                 return 1;
-//             }
-//             // Diagonal Win
-//             else if (arr[1] == arr[5] && arr[5] == arr[9])
-//             {
-//                 return 1;
-//             }
-//             else if (arr[3] == arr[5] && arr[5] == arr[7])
-//             {
-//                 return 1;
-//             }
-//             // Draw
-//             else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
-//             {
-//                 return -1;
-//             }
-//             else
-//             {
-//                 return 0;
-//             }
-//         }
-//     }
-// }
