6e1326bec11eb9dae0f1536c1fc76c92ce87cb89
Tyler Morgan <tylermorgan@Tylers-MacBook-Air.local>
2022-11-04 09:05:37 -0500

    Trying to work

--- myPlayground/Program.cs
+++ myPlayground/Program.cs
@@ -1,101 +1,26 @@
 ﻿using System;
 
-namespace Playground
-{
-    class Glass
-    {
-        public int LiquidLevel;
+namespace Hello{
+    class Program{
+        static void Main(string[] args){
+        
+        Console.Clear();
+        
+        Computer comp = new Computer{
+            Brand = "Apple",
+            Year = 2022,
+            Model = "M2 MacBook Air"
+        };
 
-        public void Drink(int milliliters)
-        {
-            LiquidLevel -= milliliters;
-        }
-
-        public void Refill()
-        {
-            if (LiquidLevel < 100)
-            {
-                LiquidLevel = 200;
-            }
-        }
-
-    }//Glass
+        Console.WriteLine($"My new computer is a {comp.Year} {comp.Brand} {comp.Model}.");
 
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.Clear();
-
-            Glass glass = new Glass();
-            glass.LiquidLevel = 200;
-            bool stopped = false;
-
-            while (!stopped)
-            {
-                // Console.WriteLine("drink, print, exit");
-                string command = Console.ReadLine();
-
-                string[] divide = command.Split(' ');
-
-                switch (divide[0])
-                {
-
-                    case "drink":
-
-                        string number = divide[1];
-                        int milliliters = Convert.ToInt32(number);
-
-                        glass.Drink(milliliters);
-                        if (glass.LiquidLevel < 100)
-                            glass.Refill();
-                        break;
-
-                    case "print":
-
-                        if (glass.LiquidLevel >= 151 && glass.LiquidLevel < 200)
-                        {
-                            Console.WriteLine(@"
-|  |
-|##|
-|##|
-|##|
-
-
-                                ");
-                        }
-                        else if (glass.LiquidLevel <= 150 && glass.LiquidLevel >= 100)
-                        {
-                            Console.WriteLine(@"
-|  |
-|  |
-|##|
-|##|                   
-                                                    
-                                ");
-                        }
-                        else
-                        {
-                            Console.WriteLine(@"
-|##|
-|##|
-|##|
-|##|
-
-                            ");
-                        }
-
-                        // Console.WriteLine($"This glass contains {glass.LiquidLevel} milliliters of liquid.");
-                        break;
-
-                    case "stop":
-
-                        stopped = true;
-                        break;
-
-                }//switch
-            }//while
         }//Main
-    }//Program
+
+        class Computer{
+            public string Brand;
+            public int Year;
+            public string Model;
+        }
+    }
 }
