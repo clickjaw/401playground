17b58805450527bae16046c60119f91ed53a8967
Clickjaw <clickjaw@outlook.com>
2022-11-07 08:04:32 -0600

    working

--- myPlayground/Program.cs
+++ myPlayground/Program.cs
@@ -1,26 +1,61 @@
 ﻿using System;
 
-namespace Hello{
-    class Program{
-        static void Main(string[] args){
-        
-        Console.Clear();
-        
-        Computer comp = new Computer{
-            Brand = "Apple",
-            Year = 2022,
-            Model = "M2 MacBook Air"
-        };
+namespace Project
+{
 
-        Console.WriteLine($"My new computer is a {comp.Year} {comp.Brand} {comp.Model}.");
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
 
 
-        }//Main
+        private int _age;
 
-        class Computer{
-            public string Brand;
-            public int Year;
-            public string Model;
+        public Person(string firstName, string lastName, int age)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _age = age;
+        }
+
+
+
+        public void SetName()
+        {
+            Console.WriteLine($"{_firstName} {_lastName}: age {_age} - created.");
+        }
+
+
+    }
+
+    class Car
+    {
+
+        private string _model;
+        private string _make;
+        public Car(string model, string make)
+        {
+            _model = model;
+            _make = make;
+        }
+        public void CallCar()
+        {
+            Console.WriteLine($"His car is a {_model} {_make}.");
         }
     }
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparing to make a person.");
+
+            Person tyler = new Person("Tyler", "Morgan", 33);
+            Car myCar = new Car("Honda", "Fit");
+
+            tyler.SetName();
+            myCar.CallCar();
+
+
+        }//main
+    }//program
+}//namespace
